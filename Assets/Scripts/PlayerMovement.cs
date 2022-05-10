c2cf27ba75bf30c3b99f529df02ce56d87f47394
David Gago <davidopg@gmail.com>
2022-05-10 20:30:29 +0100

    added player. closes #8

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float speed = 10f;
+    private float vertical;
+    private float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontal * Time.deltaTime * speed, vertical * Time.deltaTime * speed);
+    }
+}
