d76d6efb5a43f32d6561c574b33f418eeae40b4a
Malwina Tałan <malwina.tal@gmail.com>
2022-05-10 23:36:28 +0100

    * added moving Rocks

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -2,25 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleRotation : MonoBehaviour
+public class ObstacleMovement : MonoBehaviour
 {
     public int minRange;
     public int maxRange;
-    
+    public float speed = 150;
+
     private Rigidbody2D body;
-    
-    float degreesPerSecond;
+    private float degreesPerSecond;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         degreesPerSecond = Random.Range(minRange, maxRange);
+        body.AddForce(Vector2.left * speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, degreesPerSecond) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, degreesPerSecond * Time.deltaTime));
     }
 }
