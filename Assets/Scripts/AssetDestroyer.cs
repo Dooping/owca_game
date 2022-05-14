e873c09ac653c3b1d2f5826d023110bea5655a19
Malwina Tałan <malwina.tal@gmail.com>
2022-05-14 15:51:17 +0100

    * added asset destroyer, closes #17

---
+++ Assets/Scripts/AssetDestroyer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetDestroyer : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "AssetKiller")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
