c3c9aefe83af9eff11a4a974f7237e489870e063
David Gago <davidopg@gmail.com>
2022-05-19 23:13:10 +0100

    fixed music bug, closes #38

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,9 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private Value difficulty;
     public void LoadGame()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    private void OnEnable()
+    {
+        difficulty.value = 0;
     }
 
 }
